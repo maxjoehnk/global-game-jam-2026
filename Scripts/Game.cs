f113dd3b9134b9a602a507092dfa6500f5d4de14
Max Jöhnk <maxjoehnk@gmail.com>
2026-01-30 22:58:57 +0100

    [wip] add level finish

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -8,9 +8,9 @@ public partial class Game : Node2D
 {
 	private const int PhysicsBaseLayer = 8;
 
-    private Node2D LayerContainer => GetNode<Node2D>("Layers");
-    private Player Player => GetNode<Player>("Player");
-    private GameHud Hud => GetNode<GameHud>("UI/HUD");
+	private Node2D LayerContainer => GetNode<Node2D>("Layers");
+	private Player Player => GetNode<Player>("Player");
+	private GameHud Hud => GetNode<GameHud>("UI/HUD");
 
 	private int LayerCount => LayerContainer.GetChildCount();
 
@@ -19,35 +19,35 @@ public partial class Game : Node2D
 	private Node2D ActiveLayer => LayerContainer.GetChild<Node2D>(this.activeLayerIndex);
 	private uint ActivePhysicsLayer => (uint)1 << (PhysicsBaseLayer + this.activeLayerIndex);
 
-    public override void _Ready()
-    {
-        int physicsLayer = PhysicsBaseLayer;
-        Array<Node> layers = this.LayerContainer.GetChildren();
-        this.Hud.SetLayers(new Array<string>(layers.Select(l => l.Name.ToString())));
-        foreach (Node child in layers)
-        {
-            foreach (PhysicsBody2D physicsBody2D in child.FindChildren("*", type: nameof(PhysicsBody2D))
-                         .Where(c => c is PhysicsBody2D)
-                         .Cast<PhysicsBody2D>())
-            {
-                physicsBody2D.CollisionLayer = (uint)1 << physicsLayer;
-            }
-            physicsLayer++;
-        }
-        this.UpdateActiveLayer();
-    }
+	public override void _Ready()
+	{
+		int physicsLayer = PhysicsBaseLayer;
+		Array<Node> layers = this.LayerContainer.GetChildren();
+		this.Hud.SetLayers(new Array<string>(layers.Select(l => l.Name.ToString())));
+		foreach (Node child in layers)
+		{
+			foreach (PhysicsBody2D physicsBody2D in child.FindChildren("*", type: nameof(PhysicsBody2D))
+				         .Where(c => c is PhysicsBody2D)
+				         .Cast<PhysicsBody2D>())
+			{
+				physicsBody2D.CollisionLayer = (uint)1 << physicsLayer;
+			}
+
+			physicsLayer++;
+		}
+
+		this.UpdateActiveLayer();
+	}
 
 	public override void _Process(double delta)
 	{
 		if (Input.IsActionJustPressed(InputAction.LayerUp))
 		{
-			GD.Print("Next Layer");
 			this.NextLayer();
 		}
 
 		if (Input.IsActionJustPressed(InputAction.LayerDown))
 		{
-			GD.Print("Previous Layer");
 			this.PreviousLayer();
 		}
 	}
@@ -82,7 +82,7 @@ public partial class Game : Node2D
 			layer.Modulate = i == this.activeLayerIndex ? Color.FromHsv(0, 0, 1) : Color.FromHsv(0, 0, 1, 0.25f);
 		}
 
-        Player.SetActiveCollisionLayer(this.ActivePhysicsLayer);
-        this.Hud.SetActiveLayer(this.activeLayerIndex);
-    }
+		Player.SetActiveCollisionLayer(this.ActivePhysicsLayer);
+		this.Hud.SetActiveLayer(this.activeLayerIndex);
+	}
 }
