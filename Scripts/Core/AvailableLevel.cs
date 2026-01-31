95b92e7fa88647579852b0d1f4deaae58a98f358
Max Jöhnk <maxjoehnk@gmail.com>
2026-01-31 16:34:20 +0100

    improve level loading

--- Scripts/Core/AvailableLevel.cs
+++ Scripts/Core/AvailableLevel.cs
@@ -1,14 +1,48 @@
+using System.Text.RegularExpressions;
+
 namespace GlobalGameJam.Scripts.Core;
 
-public class AvailableLevel
+public partial class AvailableLevel
 {
+	private const int FileIndexOffset = 1000;
+	private const int TestIndexOffset = -1000;
+
 	public string Path { get; }
 
 	public string Name { get; }
 
-	public AvailableLevel(string path)
+	public int LevelIndex { get; }
+
+	/// <summary>
+	/// Level that is only available in Debug Builds.
+	///
+	/// Prefix Level Files with "test_" to mark them as test levels.
+	/// </summary>
+	public bool IsTestLevel { get; set; }
+
+	public bool IsUnlocked { get; set; }
+
+	public AvailableLevel(string path, int fileIndex)
 	{
 		this.Path = path;
-		this.Name = path.Replace(".tscn", "");
+		Match match = LevelNameRegex().Match(path);
+		this.Name = match.Groups["Name"].Value;
+		this.IsTestLevel = match.Groups["IsTest"].Success;
+		if (match.Groups["Id"].Success)
+		{
+			this.LevelIndex = int.Parse(match.Groups["Id"].Value);
+		}
+		else if (this.IsTestLevel)
+		{
+			this.LevelIndex = TestIndexOffset + fileIndex;
+		}
+		else
+		{
+			this.LevelIndex = FileIndexOffset + fileIndex;
+		}
 	}
+
+	[GeneratedRegex("(?<IsTest>test_)?((?<Id>[0-9]+)_)?(?<Name>.*).tscn",
+		RegexOptions.Compiled | RegexOptions.IgnoreCase)]
+	private static partial Regex LevelNameRegex();
 }
