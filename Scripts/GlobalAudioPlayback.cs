95a79a6c7b27c31d51432765b7af022d1e83cf85
Jonathan Kortjohann <jonathan.kortjohann@topas.tech>
2026-02-01 15:43:23 +0100

    Add Music to levels and shredder sound

--- Scripts/GlobalAudioPlayback.cs
+++ Scripts/GlobalAudioPlayback.cs
@@ -4,30 +4,30 @@ using System.Linq;
 
 public partial class GlobalAudioPlayback : Node
 {
-    public static GlobalAudioPlayback Instance { get; private set; } = null!;
+	public static GlobalAudioPlayback Instance { get; private set; } = null!;
 
-    public override void _Ready()
-    {
-        Instance = this;
-    }
+	public override void _Ready()
+	{
+		Instance = this;
+	}
 
-    public void StopAllPlayback()
-    {
-        foreach (AudioStreamPlayer audioStreamPlayer in this.GetChildren().Cast<AudioStreamPlayer>())
-        {
-            audioStreamPlayer.Stop();
-        }
-    }
+	public void StopAllPlayback()
+	{
+		foreach (AudioStreamPlayer audioStreamPlayer in this.GetChildren().Cast<AudioStreamPlayer>())
+		{
+			audioStreamPlayer.Stop();
+		}
+	}
 
-    public void StartAllPlayback()
-    {
-        foreach (AudioStreamPlayer audioStreamPlayer in this.GetChildren().Cast<AudioStreamPlayer>())
-        {
-            if (audioStreamPlayer.Playing)
-            {
-                continue;
-            }
-            audioStreamPlayer.Play();
-        }
-    }
+	public void StartAllPlayback()
+	{
+		foreach (AudioStreamPlayer audioStreamPlayer in this.GetChildren().Cast<AudioStreamPlayer>())
+		{
+			if (audioStreamPlayer.Playing)
+			{
+				continue;
+			}
+			audioStreamPlayer.Play();
+		}
+	}
 }
